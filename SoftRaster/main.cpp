7f48392f8659c3b7e2c4fb8d9f53687ebabf28c6
王安康 <jecced@gmail.com>
2022-09-27 21:38:13 +0800

    feat: 实例化自定义窗体

--- SoftRaster/main.cpp
+++ SoftRaster/main.cpp
@@ -1,6 +1,1 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+#include <iostream>#include <windows.h>#include "FrameWindow.h"int WINAPIWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {    FrameWindow w1{};    w1.height = 600;    w1.width = 800;    std::cout << "hello world" << std::endl;    return w1.run(hInstance, hPrevInstance, lpCmdLine, nCmdShow);}
