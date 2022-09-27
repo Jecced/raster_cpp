ddcb7ced88aa3b8013bd595fa437ad8976cd4f88
王安康 <jecced@gmail.com>
2022-09-27 21:38:00 +0800

    feat: 创建一个窗体类

---
+++ SoftRaster/FrameWindow.h
@@ -0,0 +1,19 @@
+//
+// Created by root on 2022/9/27.
+//
+
+#ifndef SOFTRASTER_FRAMEWINDOW_H
+#define SOFTRASTER_FRAMEWINDOW_H
+
+#include <windows.h>
+
+class FrameWindow {
+public:
+    int width;
+    int height;
+
+    int run(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow);
+};
+
+
+#endif //SOFTRASTER_FRAMEWINDOW_H
