9da551a27d43367ba6b2156faf00bebecbebf8d1
王安康 <jecced@gmail.com>
2022-09-28 13:27:55 +0800

    refactor: 修改类名

--- SoftRaster/main.cpp
+++ SoftRaster/main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <windows.h>
 #include "FrameWindow.h"
-#include "math/Vec2.h"
-
+#include "math/vec2.h"
 
 int WINAPI
 WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
@@ -23,5 +22,6 @@ int main() {
     a = b.clone();
     std::cout << a.x << std::endl;
     std::cout << a.y << std::endl;
+
     return 0;
 }
