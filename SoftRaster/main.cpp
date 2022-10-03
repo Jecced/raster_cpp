2bb9382394bcc110a68e4923c48102a9b3ae55cc
王安康 <jecced@gmail.com>
2022-10-03 14:58:58 +0800

    feat: 移除废弃窗体实现

--- SoftRaster/main.cpp
+++ SoftRaster/main.cpp
@@ -1,20 +1,5 @@
 #include <iostream>
-#include <windows.h>
-#include "FrameWindow.h"
 #include "matrix/matrix.h"
-
-int WINAPI
-WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
-
-    FrameWindow *w1 = new FrameWindow(800, 600);
-
-    std::cout << "hello world" << std::endl;
-
-    int exitCode = w1->run(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
-
-    delete w1;
-    return exitCode;
-}
 
 int main() {
 //    std::cout << "hello" << std::endl;
