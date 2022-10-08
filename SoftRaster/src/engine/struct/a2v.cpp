dc89ae1cdf38c1930d371f92cff8f5f1bb864ecb
王安康 <jecced@gmail.com>
2022-10-08 21:54:41 +0800

    feat: a2v结构体

---
+++ SoftRaster/src/engine/struct/a2v.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Admin on 2022/10/8.
+//
+
+#include "a2v.h"
---
+++ SoftRaster/src/engine/struct/a2v.h
@@ -0,0 +1,19 @@
+//
+// Created by Admin on 2022/10/8.
+//
+
+#ifndef SOFTRASTER_A2V_H
+#define SOFTRASTER_A2V_H
+
+
+#include "../../matrix/matrix.h"
+
+class A2V {
+public:
+    Vec3 vertex;
+    Vec3 normal;
+    Vec2 texCoord;
+};
+
+
+#endif //SOFTRASTER_A2V_H
