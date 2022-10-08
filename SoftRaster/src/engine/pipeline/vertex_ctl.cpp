7cfc00ce1133beb35a4a6b88011e7e01f48e5acf
王安康 <jecced@gmail.com>
2022-10-08 21:55:39 +0800

    feat: 创建一个空的顶点控制器

---
+++ SoftRaster/src/engine/pipeline/vertex_ctl.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Admin on 2022/10/8.
+//
+
+#include "vertex_ctl.h"
---
+++ SoftRaster/src/engine/pipeline/vertex_ctl.h
@@ -0,0 +1,15 @@
+//
+// Created by Admin on 2022/10/8.
+//
+
+#ifndef SOFTRASTER_VERTEX_CTL_H
+#define SOFTRASTER_VERTEX_CTL_H
+
+
+class vertex_ctl {
+public:
+    void pushMesh();
+};
+
+
+#endif //SOFTRASTER_VERTEX_CTL_H
