0cbb0b4be240589914bf41bd4356ad1c27e8d09b
王安康 <jecced@gmail.com>
2022-10-15 17:47:32 +0800

    feat: 创建管线流程控制的类

---
+++ SoftRaster/src/engine/pipeline/pipeline.cpp
@@ -0,0 +1,5 @@
+//
+// Created by root on 2022/10/15.
+//
+
+#include "pipeline.h"
---
+++ SoftRaster/src/engine/pipeline/pipeline.h
@@ -0,0 +1,14 @@
+//
+// Created by root on 2022/10/15.
+//
+
+#ifndef SOFTRASTER_PIPELINE_H
+#define SOFTRASTER_PIPELINE_H
+
+
+class Pipeline {
+
+};
+
+
+#endif //SOFTRASTER_PIPELINE_H
