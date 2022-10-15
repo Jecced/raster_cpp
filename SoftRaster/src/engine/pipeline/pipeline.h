aa2629ccde8f200a353234f34c408abaef318486
王安康 <jecced@gmail.com>
2022-10-15 23:18:44 +0800

    feat: 管线流程

--- SoftRaster/src/engine/pipeline/pipeline.h
+++ SoftRaster/src/engine/pipeline/pipeline.h
@@ -6,8 +6,33 @@
 #define SOFTRASTER_PIPELINE_H
 
 
-class Pipeline {
+#include "vertex_ctl.h"
+#include "../comp/camera.h"
+#include "../buffer/camera_target.h"
+#include "../shader/shader_lab.h"
 
+class Pipeline {
+private:
+    int width, height;
+    VertexCtl *vertexCtl;
+    CameraTarget *cameraTarget;
+    ShaderLab *shader;
+    V2F *variableV2F;
+    Vec4 *hsCache[3] = {vec4(0), vec4(0), vec4(0)};
+    Vec4 *boundBox;
+public:
+    Pipeline(int width, int height, unsigned int *framebuffer);
+
+    void setupCamera(Camera camera);
+
+    void drawMesh(const Mesh &mesh);
+
+    void bindShader(ShaderLab &shader);
+
+    void clear();
+
+private:
+    void rasterizer(Vec4 &p0, Vec4 &p1, Vec4 &p2, const V2F &v0, const V2F &v1, const V2F &v2);
 };
 
 
